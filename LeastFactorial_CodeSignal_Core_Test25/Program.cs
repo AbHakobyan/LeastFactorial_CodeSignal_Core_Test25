d2ce80f93906d891a3e9a081174b483fcffd90f8
Albert Hakobyan <abhakobyan24@gmail.com>
2021-08-27 12:01:47 +0400

    Добавьте файлы проекта.

---
+++ LeastFactorial_CodeSignal_Core_Test25/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeastFactorial_CodeSignal_Core_Test25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = leastFactorial(17);
+            Console.WriteLine(n);
+        }
+
+        static int leastFactorial(int n)
+        {
+            int count = 1;
+            for (int i = 1; i < n; i++)
+            {
+                if (count < n)
+                {
+                    count *= i;
+                    continue;
+                }
+            }
+            return count;
+        }
+    }
+}
